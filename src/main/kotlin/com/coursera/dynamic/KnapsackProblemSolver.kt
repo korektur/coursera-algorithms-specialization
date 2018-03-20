95e55694b55666b748e37eb187d173ef93b14aac
Ruslan Akhundov <akhundov2@gmail.com>
2018-03-20 23:00:45 +0100

    +knapsack solver with much less memory consumption

--- src/main/kotlin/com/coursera/dynamic/KnapsackProblemSolver.kt
+++ src/main/kotlin/com/coursera/dynamic/KnapsackProblemSolver.kt
@@ -1,20 +1,23 @@
 package com.coursera.dynamic
 
+import java.util.*
 import kotlin.math.max
 
 public data class Item(public val weight: Int, public val value: Long)
 
 public fun solve(capacity: Int, items: List<Item>): Long {
-    val d = Array(items.size + 1, { LongArray(capacity + 1) })
+    val d = LinkedList(listOf(LongArray(capacity + 1), LongArray(capacity + 1)))
     for (k in 1..items.size) {
         for (s in 1..capacity) {
             if (s >= items[k - 1].weight) {
-                d[k][s] = max(d[k - 1][s], d[k - 1][s - items[k - 1].weight] + items[k - 1].value)
+                d[1][s] = max(d[0][s], d[0][s - items[k - 1].weight] + items[k - 1].value)
             } else {
-                d[k][s] = d[k - 1][s]
+                d[1][s] = d[0][s]
             }
         }
+
+        System.arraycopy(d[1], 0, d[0], 0, d[1].size)
     }
 
-    return d[items.size][capacity]
+    return d[1][capacity]
 }
