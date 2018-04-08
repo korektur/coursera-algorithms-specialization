9884b10405dc2d9405fec18794101738bd537124
Ruslan Akhundov <akhundov2@gmail.com>
2018-04-08 22:49:13 +0200

    +travelling salesman problem solver

---
+++ src/main/kotlin/com/coursera/graph/path/np/TravelingSalesmanProblemSolver.kt
@@ -0,0 +1,57 @@
+package com.coursera.graph.path.np
+
+import com.google.common.math.IntMath.pow
+import java.lang.Long.min
+import java.lang.Long.toString
+
+class TravelingSalesmanProblemSolver(val graph: Array<DoubleArray>) {
+
+    companion object {
+        public val INF = Double.MAX_VALUE
+        private var counter = 0L
+    }
+
+    private val n = graph.size
+    private val maxMask = pow(2, n)
+    private val d = Array(n, { DoubleArray(maxMask) })
+
+    public suspend fun evaluate(): Double {
+        for (i in 0 until n) {
+            for (mask in 0 until maxMask) {
+                d[i][mask] = INF
+                if (mask == 0) {
+                    d[i][mask] = 0.0
+                }
+            }
+        }
+
+        d[0][0] = 0.0
+        val findCheapest = findCheapest(0, (1 shl n) - 1, 0)
+        return findCheapest
+    }
+
+    private fun findCheapest(id: Int, mask: Int, depth: Int): Double {
+        ++counter
+        if (d[id][mask] != INF) {
+            return d[id][mask]
+        }
+
+        for (j in 0 until n) {
+            if (j == id) {
+                continue
+            }
+
+            val marker = 1 shl j // 2^j
+            if (graph[id][j] != INF && mask.and(marker) == marker) {
+                val findCheapest = findCheapest(j, mask.xor(marker), depth + 1)
+                if (findCheapest != INF) {
+                    val newDist = (findCheapest + graph[id][j]) % Int.MAX_VALUE
+                    d[id][mask] = kotlin.math.min(d[id][mask], newDist)
+
+                }
+            }
+        }
+
+        return d[id][mask]
+    }
+}
---
+++ src/test/kotlin/com/coursera/graph/path/np/TravelingSalesmanProblemSolverTest.kt
@@ -0,0 +1,143 @@
+package com.coursera.graph.path.np
+
+import com.coursera.graph.Edge
+import com.coursera.graph.Graph
+import com.coursera.graph.Vertex
+import com.coursera.graph.clustering.ClusterEvaluator
+import com.coursera.graph.path.np.TravelingSalesmanProblemSolver.Companion.INF
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.runBlocking
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.lang.Math.pow
+import java.lang.Math.sqrt
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.stream.Collectors.toList
+
+
+class TravelingSalesmanProblemSolverTest {
+
+    companion object {
+        const val FILE_NAME = "src/test/resources/tsp.txt"
+    }
+
+    @Test
+    fun testTspSmallCliqueGraph() {
+        val inputGraph = arrayOf(doubleArrayOf(0.0, 1.0, 2.0, 1.0),
+                doubleArrayOf(1.0, 0.0, 1.0, 2.0),
+                doubleArrayOf(2.0, 1.0, 0.0, 1.0),
+                doubleArrayOf(1.0, 2.0, 1.0, 0.0))
+
+        val tspSolver = TravelingSalesmanProblemSolver(inputGraph)
+
+        val result = runBlocking {
+            tspSolver.evaluate()
+        }
+
+        assertEquals(4, result.toInt())
+    }
+
+    @Test
+    fun testTspSmallGraph() {
+        val inputGraph = arrayOf(doubleArrayOf(0.0, 10.0, 20.0, 15.0),
+                doubleArrayOf(10.0, 0.0, 35.0, 25.0),
+                doubleArrayOf(15.0, 35.0, 0.0, 30.0),
+                doubleArrayOf(20.0, 25.0, 30.0, 0.0))
+
+        val tspSolver = TravelingSalesmanProblemSolver(inputGraph)
+
+        val result = runBlocking {
+            tspSolver.evaluate()
+        }
+
+        assertEquals(80, result.toInt())
+    }
+
+
+    @Test
+    fun testTspSmallPathGraph() {
+        val inputGraph = arrayOf(
+                doubleArrayOf(0.0, 1.0, INF, 1.0),
+                doubleArrayOf(1.0, 0.0, 1.0, INF),
+                doubleArrayOf(INF, 1.0, 0.0, 1.0),
+                doubleArrayOf(1.0, INF, 1.0, 0.0))
+
+        val tspSolver = TravelingSalesmanProblemSolver(inputGraph)
+
+        val result = runBlocking { tspSolver.evaluate() }
+
+        assertEquals(4, result.toInt())
+    }
+
+    private data class Point(val x: Double, val y: Double) {
+        fun distTo(other: Point): Double {
+            return sqrt(pow(x - other.x, 2.0) + pow(y - other.y, 2.0))
+        }
+    }
+
+    @Test
+    fun testTspBigGraph() {
+        val input = Files.lines(Paths.get(FILE_NAME))
+                .skip(1)
+                .map { it.split(" ") }
+                .map { Point(it[0].toDouble(), it[1].toDouble()) }
+                .collect(toList())
+
+        val fstMedianPoint = input[12]
+        val sndMedianPoint = input[13]
+        val leftPart = input.stream()
+                .filter { it.x <= fstMedianPoint.x }
+                .collect(toList())
+
+        val rightPart = input.stream()
+                .filter { it.x >= sndMedianPoint.x }
+                .collect(toList())
+
+        println(rightPart.size)
+        println(leftPart.size)
+        val leftMatrix = toEdgeMatrix(leftPart)
+        val rightMatrix = toEdgeMatrix(rightPart)
+
+
+        val leftResult = async {
+            println("rightPart")
+            TravelingSalesmanProblemSolver(leftMatrix).evaluate()
+        }
+
+        val rightResult = async {
+            println("leftPart")
+            TravelingSalesmanProblemSolver(rightMatrix).evaluate()
+        }
+
+        runBlocking {
+            println(leftResult.await())
+            println(rightResult.await())
+
+            println(leftResult.await() + rightResult.await())
+            val ans = leftResult.getCompleted() + rightResult.getCompleted() - fstMedianPoint.distTo(input[11]) - sndMedianPoint.distTo(input[14]) +
+                    fstMedianPoint.distTo(sndMedianPoint) + input[11].distTo(input[14])
+            println(ans)
+        }
+
+        println(runBlocking { TravelingSalesmanProblemSolver(toEdgeMatrix(input)).evaluate() }
+
+        )
+    }
+
+
+    private fun toEdgeMatrix(points: List<Point>): Array<DoubleArray> {
+        val n = points.size
+        val edgeMatrix = Array(n, { DoubleArray(n) })
+        for (i in 0 until n) {
+            for (j in i + 1 until n) {
+                val dist = points[i].distTo(points[j])
+                edgeMatrix[i][j] = dist
+                edgeMatrix[j][i] = dist
+            }
+        }
+
+        return edgeMatrix
+    }
+}
